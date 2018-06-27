f0197a00a21d3c93602288f5632d8a674eb3bf52
Mark O'Brien <markob@uw.edu>
2018-06-27 13:24:18 -0700

    single donor view, run on *nix using env, perms update, security concern/fix loopback only for macos
    	modified:   main.py
    	modified:   setup.py

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-import os
+#!/usr/bin/env python3
+
 import base64
+import os
+import platform
 
 from flask import Flask, render_template, request, redirect, url_for, session
+from model import Donation, Donor
 
-from model import Donation 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/donations/<donor_name>')
+def single_donor(donor_name):
+    donations = Donation().select().join(Donor).where(Donor.name == donor_name)
+    return render_template('donations.jinja2', donations=donations)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
-
+    # enable 0/0 for heroku
+    if platform.system() == 'Darwin':
+        # Running on development laptop (do not expose external port).
+        # heroku is not expected to run using MacOS
+        app.run(host='127.0.0.1', port=port)
+    else:
+        app.run(host='0.0.0.0', port=port)
